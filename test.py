2e644b52391d7db60e64f9fc74c07beba7ae56b0
Barry Wainstock <barrywainstock@gmail.com>
2016-07-30 18:50:22 -0700

    flaskr redo

--- test.py
+++ test.py
@@ -1,23 +1,53 @@
-import travistest
 import unittest
+import os
+import tempfile
+import flaskr
 
-class TravisTestCase(unittest.TestCase):
-
-    def setUp(self):
-        travistest.app.config['TESTING'] = True
-        self.app = travistest.app.test_client()
+class FlaskrTestCase(unittest.TestCase):
     
-    def test_index(self):
+    def setUp(self):
+        self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+        flaskr.app.config['TESTING'] = True
+        self.app = flaskr.app.test_client()
+        with flaskr.app.app_context():
+            flaskr.init_db()
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(flaskr.app.config['DATABASE'])
+
+    def test_empty_db(self):
         rv = self.app.get('/')
-        assert b'Hello world' in rv.data
+        assert b'No entries here so far' in rv.data
 
-    def test_nonexistant_endpoint(self):
-        rv = self.app.get('/butt')
-        assert rv.status_code > 200
+    def login(self, username, password):
+        return self.app.post('/login', data=dict(
+            username=username,
+            password=password
+        ), follow_redirects=True)
 
-    def test_failing_test(self):
-        rv = self.app.get('/butt')
-        assert b'Hello world' not in rv.data
+    def logout(self):
+        return self.app.get('/logout', follow_redirects=True)
+
+    def test_login_logout(self):
+        rv = self.login('admin', 'password')
+        assert b'You are now logged in' in rv.data
+        rv = self.logout()
+        assert b'You are now logged out' in rv.data
+        rv = self.login('admin', 'wrongpassword')
+        assert b'Invalid password' in rv.data
+        rv = self.login('wronguser', 'password')
+        assert b'Invalid username' in rv.data
+
+    def test_messages(self):
+        self.login('admin', 'password')
+        rv = self.app.post('/add', data=dict(
+            title='<Hello>',
+            text='<strong>HTML</strong> allowed here'
+        ), follow_redirects=True)
+        assert b'No entries here so far' not in rv.data
+        assert b'&lt;Hello&gt;' in rv.data
+        assert b'<strong>HTML</strong> allowed here' in rv.data
 
 if __name__ == '__main__':
-    unittest.main()    
+    unittest.main()
