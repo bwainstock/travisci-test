62792070cac5e8af6139cf88f60e70e05f6f0617
Barry Wainstock <barrywainstock@gmail.com>
2016-08-29 21:39:42 -0700

    Initial lambda deploy test

--- README.md
+++
@@ -1,3 +0,0 @@
-# travisci-test
-[![Build Status](https://travis-ci.org/bwainstock/travisci-test.svg?branch=master)](https://travis-ci.org/bwainstock/travisci-test)
-A repo to test travis ci integration
--- test.py
+++ test.py
@@ -1,53 +1,5 @@
 import unittest
-import os
-import tempfile
-import flaskr
 
-class FlaskrTestCase(unittest.TestCase):
-    
-    def setUp(self):
-        self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
-        flaskr.app.config['TESTING'] = True
-        self.app = flaskr.app.test_client()
-        with flaskr.app.app_context():
-            flaskr.init_db()
-
-    def tearDown(self):
-        os.close(self.db_fd)
-        os.unlink(flaskr.app.config['DATABASE'])
-
-    def test_empty_db(self):
-        rv = self.app.get('/')
-        assert b'No entries here so far' in rv.data
-
-    def login(self, username, password):
-        return self.app.post('/login', data=dict(
-            username=username,
-            password=password
-        ), follow_redirects=True)
-
-    def logout(self):
-        return self.app.get('/logout', follow_redirects=True)
-
-    def test_login_logout(self):
-        rv = self.login('admin', 'password')
-        assert b'You are now logged in' in rv.data
-        rv = self.logout()
-        assert b'You are now logged out' in rv.data
-        rv = self.login('admin', 'wrongpassword')
-        assert b'Invalid password' in rv.data
-        rv = self.login('wronguser', 'password')
-        assert b'Invalid username' in rv.data
-
-    def test_messages(self):
-        self.login('admin', 'password')
-        rv = self.app.post('/add', data=dict(
-            title='<Hello>',
-            text='<strong>HTML</strong> allowed here'
-        ), follow_redirects=True)
-        assert b'No entries here so far' not in rv.data
-        assert b'&lt;Hello&gt;' in rv.data
-        assert b'<strong>HTML</strong> allowed here' in rv.data
-
-if __name__ == '__main__':
-    unittest.main()
+class TheChapelTestCase(unittest.TestCase):
+    def test_true(self):
+        self.assertEqual(1,1)
